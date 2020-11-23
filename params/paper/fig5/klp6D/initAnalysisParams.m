ff4a585b098fb70877d7afe8050f2f2233821736
Saad Ansari <saadjansari@saads-mbp.lan>
2020-11-23 12:19:44 -0500

    New param files for monopolar strains wt,klp6d,alp14d,cls1-36

---
+++ params/paper/fig5/klp6D/initAnalysisParams.m
@@ -0,0 +1,17 @@
+function params = initAnalysisParams();
+
+    % default parent path where fitting results will exist, unless a full path is provided to analysis function
+    params.pathParent = '/Users/saadjansari/Documents/Projects/ImageAnalysis/SingleCell/Results/Paper';
+
+    % Cell type. Mitotic analysis differs from interphase analysis.
+    params.cellType = 'Monopolar';
+
+    % channelsToAnalyze gives the channel index where channelFeatures are located
+    params.channelsToAnalyze = [ 1];
+    params.channelFeatures = {'Microtubule'};
+    params.timeStep = 8; % Can be overwritten in analysis code
+
+    params.flagMovie = 1;
+    params.flagGraph = 0;
+
+end
---
+++ params/paper/fig5/wt/initParams.m
@@ -0,0 +1,39 @@
+function paramsPath = initParams( opts)
+    % Creates the settings for running the software
+    
+    % Initialize Configuration, Cell Info and Paths
+    if exist( fullfile( pwd, 'initConfiguration.m') ) ~= 2
+        error( ['copy initConfiguration.m to the current folder location : ', pwd] );
+    else
+        params = initConfiguration( opts);
+    end
+
+    % Set up the cell info for fitting
+    if exist( fullfile( pwd, 'initCellInfo.m') ) ~= 2
+        error( ['copy initCellInfo.m to the current folder location : ', pwd] );
+    else
+        params.cellInfo = initCellInfo( opts);
+    end
+
+    % Set up the paths for running single cell
+    if exist( fullfile( pwd, 'initPaths.m') ) ~= 2
+        error( ['copy initPaths.m to the current folder location : ', pwd] );
+    else
+        params = initPaths( params);
+    end
+
+    numCells = length( params);
+    paramsRef = params;
+
+    for jCell = 1 : numCells
+
+        params = paramsRef{ jCell};
+
+        % Save params
+        paramsPath{ jCell} = params.paramsPath;
+        save( paramsPath{ jCell}, 'params', '-v7.3')
+
+    end
+
+
+end
